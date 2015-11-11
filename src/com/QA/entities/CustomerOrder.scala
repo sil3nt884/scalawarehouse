bf785605001ad3c7a911902cb8bb01f87a5c9475
Ricky Luu <sil3nt884@hotmail.co.uk>
2015-11-11 09:52:21 +0000

    fullying working custoemrOrder display

--- src/com/QA/entities/CustomerOrder.scala
+++ src/com/QA/entities/CustomerOrder.scala
@@ -1,6 +1,64 @@
 package com.QA.entities
 import java.util.Date
+import javafx.beans.property.SimpleStringProperty;
 
-class CustomerOrder (ID : Integer , productID : Integer, date : Date , quantity : Integer, status : Integer   ) {
+class CustomerOrder(customerid : Integer, ID: Integer, productID: Integer, date: String, quantity: Integer, status: Integer) {
   
+  val CustomerID = new SimpleStringProperty(customerid.toString())
+  val OrderID = new SimpleStringProperty(ID.toString())
+  val ProductID = new SimpleStringProperty(productID.toString())
+  val Date = new SimpleStringProperty(date.toString())
+  val Quantity = new SimpleStringProperty(quantity.toString())
+  val Status = new SimpleStringProperty(status.toString())
+
+  def setCustomerID(id: String){
+    CustomerID.set(id)
+  }
+  
+  def setOrderID(id: String){
+    OrderID.set(id)
+  }
+  
+  def setProductID(id: String) {
+    ProductID.set(id)
+  }
+
+  def setDate(date: String) {
+    Date.set(date)
+  }
+
+  def setQuantity(num: String) {
+    Quantity.set(num)
+  }
+
+  def setStatus(status: String) {
+    Status.set(status)
+  }
+  
+  def getCustomerID : String ={
+    CustomerID.get
+  }
+  
+  def getOrderID () : String ={
+     OrderID.get()
+  }
+
+  def getProductID: String = {
+    ProductID.get()
+
+  }
+ 
+
+  def getDate: String = {
+    Date.get()
+  }
+
+  def getQuantity(): String = {
+    Quantity.get()
+  }
+
+  def getStatus: String = {
+    Status.get()
+  }
+
 }
--- src/com/QA/panefx/CustomerOrderPane.scala
+++ src/com/QA/panefx/CustomerOrderPane.scala
@@ -20,15 +20,15 @@ import com.QA.entities.CustomerOrder
  * @author rluu
  */
 class CustomerOrderPane extends VBox {
-  //ID : Integer , productID : Integer, date : Date , quantity : Integer
+  //(ID : Integer , productID : Integer, date :String , quantity : Integer, status : Integer )
  
   val list = new ArrayList()
   val loadThread = new MethodThread(load)
   loadThread.start()
   loadThread.stopThread()
+  createTable()
   
-  
-  def createDialog(){
+  def createTable() {
      val data = FXCollections.observableArrayList[CustomerOrder]()
     val arraylist = list.toJavaArrayList()
 
@@ -42,17 +42,21 @@ class CustomerOrderPane extends VBox {
     
     val vbox = new VBox()
     val table = new TableView[CustomerOrder]()
-    val orderid = new TableColumn[CustomerOrder, String]("Product ID")
+    val customerid = new TableColumn[CustomerOrder, String]("CustomerID")
+    customerid.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("CustomerID"))
+    val orderid = new TableColumn[CustomerOrder, String]("OrderID")
     orderid.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("OrderID"))
-    val date = new TableColumn[CustomerOrder, String]("Employee ID")
+    val  product = new TableColumn[CustomerOrder, String]("Product ID")
+    product.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("OrderID"))
+    val date = new TableColumn[CustomerOrder, String]("Date")
+    
     date.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("Date"))
-    val qauntity = new TableColumn[CustomerOrder, String]("Qauntity ID")
+    val qauntity = new TableColumn[CustomerOrder, String]("Qauntity")
     qauntity.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("Quantity"))
     val status = new TableColumn[CustomerOrder, String]("Location")
     status.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("Status"))
     table.setItems(data)
-    table.getColumns.addAll(orderid, date, qauntity, status)
-   
+    table.getColumns.addAll(customerid,orderid, product, date, qauntity, status)
     setSpacing(5)
     setPadding(new Insets(10, 0, 0, 10));
     getChildren().addAll(table);
@@ -66,7 +70,8 @@ class CustomerOrderPane extends VBox {
       checkDatabase(result.next(), result)
       def checkDatabase(check: Boolean, rs: ResultSet) {
         if (check) {
-          list.add(new Inventory(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4))
+          println(rs.getInt(1),rs.getInt(5), rs.getInt(6), rs.getString(3), rs.getInt(7), rs.getInt(4))
+          list.add(new CustomerOrder(rs.getInt(1),rs.getInt(5), rs.getInt(6), rs.getString(3), rs.getInt(7), rs.getInt(4))
           )
           checkDatabase(rs.next(), rs)
         }

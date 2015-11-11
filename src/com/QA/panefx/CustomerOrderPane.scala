41f2a96deeb3c3bfbdb252f4eb145e99325adf01
Ricky Luu <sil3nt884@hotmail.co.uk>
2015-11-11 09:21:57 +0000

    updated

--- src/com/QA/panefx/CustomerOrderPane.scala
+++ src/com/QA/panefx/CustomerOrderPane.scala
@@ -21,11 +21,26 @@ import com.QA.entities.CustomerOrder
  */
 class CustomerOrderPane extends VBox {
   //ID : Integer , productID : Integer, date : Date , quantity : Integer
-  
+ 
+  val list = new ArrayList()
+  val loadThread = new MethodThread(load)
+  loadThread.start()
+  loadThread.stopThread()
   
   
   def createDialog(){
-      val vbox = new VBox()
+     val data = FXCollections.observableArrayList[CustomerOrder]()
+    val arraylist = list.toJavaArrayList()
+
+    for (i <- 0 until arraylist.size()) {
+      arraylist.get(i) match {
+        case line: CustomerOrder =>
+          data.add(line)
+        case _ =>
+      }
+    }
+    
+    val vbox = new VBox()
     val table = new TableView[CustomerOrder]()
     val orderid = new TableColumn[CustomerOrder, String]("Product ID")
     orderid.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("OrderID"))
@@ -35,10 +50,28 @@ class CustomerOrderPane extends VBox {
     qauntity.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("Quantity"))
     val status = new TableColumn[CustomerOrder, String]("Location")
     status.setCellValueFactory(new PropertyValueFactory[CustomerOrder, String]("Status"))
+    table.setItems(data)
     table.getColumns.addAll(orderid, date, qauntity, status)
+   
     setSpacing(5)
     setPadding(new Insets(10, 0, 0, 10));
     getChildren().addAll(table);
     
+  }
+  
+  
+   def load() {
+    val runner = new SQLRunner() {
+      val result = findAllSQL("Select * from list")
+      checkDatabase(result.next(), result)
+      def checkDatabase(check: Boolean, rs: ResultSet) {
+        if (check) {
+          list.add(new Inventory(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4))
+          )
+          checkDatabase(rs.next(), rs)
+        }
+      }
+    }
+
   }
 }
